3c133fe6ab139ed0afb24836ac8cc9215085d6c7
Bruce Smith <bruce_smith_it@fastmail.com>
2025-04-11 07:56:53 -0400

    grammar

--- crates/codebook-config/src/lib.rs
+++ crates/codebook-config/src/lib.rs
@@ -352,7 +352,7 @@ impl CodebookConfig {
         *self.regex_set.write().unwrap() = None;
     }
 
-    /// Add a word to the project config's allowlist
+    /// Add a word to the project configs allowlist
     pub fn add_word(&self, word: &str) -> Result<bool, io::Error> {
         {
             let word = word.to_ascii_lowercase();
@@ -373,7 +373,7 @@ impl CodebookConfig {
 
         Ok(true)
     }
-    /// Add a word to the global config's allowlist
+    /// Add a word to the global configs allowlist
     pub fn add_word_global(&self, word: &str) -> Result<bool, io::Error> {
         {
             let word = word.to_ascii_lowercase();
