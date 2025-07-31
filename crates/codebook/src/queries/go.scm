6a8421a0a5a6d24ad9c61ab1d2ac4da24a494de2
Elvis Henrique Pereira <elvishp2006@gmail.com>
2025-07-31 20:54:44 -0300

    feat: improve Go support (#99)
    * Add pt-BR dictionary
    
    * feat: improve Go support

--- crates/codebook/src/queries/go.scm
+++ crates/codebook/src/queries/go.scm
@@ -1,9 +1,18 @@
 (comment) @comment
-(argument_list (interpreted_string_literal) @string)
-(function_declaration (identifier) @identifier)
-(raw_string_literal) @raw_string
-(expression_list
-    (interpreted_string_literal) @string)
-(var_spec (identifier) @identifier)
+(function_declaration (identifier) @func_declaration)
+(var_spec (identifier) @var_spec)
 (short_var_declaration
-    (expression_list (identifier) @identifier))
+    (expression_list (identifier) @short_var))
+(parameter_declaration
+  name: (identifier) @parameter_name)
+(field_identifier) @field
+(type_identifier) @type_name
+(import_spec
+  name: (package_identifier) @import_alias)
+(package_clause (package_identifier) @package_name)
+(label_name) @label
+(field_declaration tag: (raw_string_literal) @struct_tag)
+(const_spec name: (identifier) @const_name)
+(range_clause left: (expression_list (identifier) @range_var))
+(interpreted_string_literal) @string_literal
+(array_type (interpreted_string_literal) @array_string)
