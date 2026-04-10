da4c68be2e86879c204ede901f5975e5e61790a4
Naphat Amundsen <47104226+Napam@users.noreply.github.com>
2026-04-10 18:12:17 +0200

    Add Norwegian dictionary (#239)
    Co-authored-by: Bo Lopker <blopker@users.noreply.github.com>

--- crates/codebook/src/dictionaries/repo.rs
+++ crates/codebook/src/dictionaries/repo.rs
@@ -150,6 +150,16 @@ static HUNSPELL_DICTIONARIES: LazyLock<Vec<HunspellRepo>> = LazyLock::new(|| {
             "https://raw.githubusercontent.com/blopker/dictionaries/refs/heads/main/dictionaries/uk/index.dic",
         ),
         HunspellRepo::new(
+            "nb_no",
+            "https://raw.githubusercontent.com/blopker/dictionaries/refs/heads/main/dictionaries/nb/index.aff",
+            "https://raw.githubusercontent.com/blopker/dictionaries/refs/heads/main/dictionaries/nb/index.dic",
+        ),
+        HunspellRepo::new(
+            "nn_no",
+            "https://raw.githubusercontent.com/blopker/dictionaries/refs/heads/main/dictionaries/nn/index.aff",
+            "https://raw.githubusercontent.com/blopker/dictionaries/refs/heads/main/dictionaries/nn/index.dic",
+        ),
+        HunspellRepo::new(
             "pt_pt",
             "https://raw.githubusercontent.com/streetsidesoftware/cspell-dicts/refs/heads/main/dictionaries/pt_PT/src/hunspell/Portuguese-European.aff",
             "https://raw.githubusercontent.com/streetsidesoftware/cspell-dicts/refs/heads/main/dictionaries/pt_PT/src/hunspell/Portuguese-European.dic",
