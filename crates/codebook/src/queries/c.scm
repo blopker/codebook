980c17385722933e98cc2fe218860e42af2a8df6
Zach Breit <zbreit18@gmail.com>
2026-05-04 16:20:43 -0400

    C: enable spelling for new language constructs (#258)
    1. Make C unit tests exhaustive for all the queries in `c.scm` (though
       not necessarily a full reflection of every C language feature).
       2. Add spell checking for macro functions and parameters: `#define
       MACRO_FUNCTION(macro_param) macro_param + 1`.
    3. Add spell checking for named unions: `union MyUnion {}`.
    4. Add spell checking for array fields and variables: `int nums[20]`.
    5. Made string checking more general. This change ensures that `string1`
       and `string2` are both spell-checked in the example
       `"string1\nstring2"`. Also string literals outside of function calls
       and initializers (e.g., during assignment) also get checked.

--- crates/codebook/src/queries/c.scm
+++ crates/codebook/src/queries/c.scm
@@ -1,13 +1,20 @@
 (comment) @comment
 (preproc_def
     name: (identifier) @identifier.constant)
+(preproc_function_def
+    name: (identifier) @identifier.function)
+(preproc_params) @identifier.parameter
 (type_definition
     declarator: (type_identifier) @identifier.type)
 (struct_specifier
     name: (type_identifier) @identifier.type)
+(union_specifier
+    name: (type_identifier) @identifier.type)
 (field_declaration
     declarator: (field_identifier) @identifier.field)
 (pointer_declarator
+    declarator: (field_identifier) @identifier.field)
+(array_declarator
     declarator: (field_identifier) @identifier.field)
 (enum_specifier
     name: (type_identifier) @identifier.type)
@@ -19,14 +26,10 @@
     declarator: (identifier) @identifier.variable)
 (declaration
     declarator: (identifier) @identifier.variable)
-(init_declarator
-    (string_literal
-        (string_content) @string))
+(array_declarator
+    declarator: (identifier) @identifier.variable)
 (function_declarator
     declarator: (identifier) @identifier.function)
 (parameter_declaration
     declarator: (identifier) @identifier.parameter)
-    (call_expression
-        (argument_list
-            (string_literal
-                [(string_content) (escape_sequence)] @string)))
+(string_content) @string
