5be7895ca57e68e5f8d921293b1eb10859d0271a
Zach Breit <zbreit18@gmail.com>
2026-05-10 15:34:31 -0400

    Fix failing test, remove duplicate tests

--- crates/codebook/tests/languages/test_c.rs
+++ crates/codebook/tests/languages/test_c.rs
@@ -1,60 +1,10 @@
-use codebook::{
-    parser::{TextRange, WordLocation},
-    queries::LanguageType,
-};
+use codebook::queries::LanguageType;
 
 use crate::{
     assert_helpers::get_marked_misspellings,
     assert_word_locations_match,
     utils::{get_processor, get_sorted_misspellings},
 };
-
-#[test]
-fn test_c_typedef() {
-    super::utils::init_logging();
-    let sample_text = r#"typedef int Mispelll;"#;
-
-    // Needs to be lexicographically sorted
-    let expected = [WordLocation::new(
-        "Mispelll".to_string(),
-        vec![TextRange {
-            start_byte: 12,
-            end_byte: 20,
-        }],
-    )];
-    let processor = super::utils::get_processor();
-    let mut misspelled = processor.spell_check(sample_text, Some(LanguageType::C), None);
-    misspelled.sort_by(|loc1, loc2| loc1.word.cmp(&loc2.word));
-    assert_eq!(misspelled, expected);
-}
-
-#[test]
-fn test_c_enum() {
-    super::utils::init_logging();
-    let sample_text = r#"enum Colrs { Grean };"#;
-
-    // Needs to be lexicographically sorted
-    let expected = [
-        WordLocation::new(
-            "Colrs".to_string(),
-            vec![TextRange {
-                start_byte: 5,
-                end_byte: 10,
-            }],
-        ),
-        WordLocation::new(
-            "Grean".to_string(),
-            vec![TextRange {
-                start_byte: 13,
-                end_byte: 18,
-            }],
-        ),
-    ];
-    let processor = super::utils::get_processor();
-    let mut misspelled = processor.spell_check(sample_text, Some(LanguageType::C), None);
-    misspelled.sort_by(|loc1, loc2| loc1.word.cmp(&loc2.word));
-    assert_eq!(misspelled, expected);
-}
 
 #[test]
 fn test_c_example_file() {
